4ef2d5f2a34160146656de290ae438c3b6739c76
allanteone <allanteone@gmail.com>
2024-03-19 22:59:44 +0200

    hw_74a updates error messages

--- src/main/java/telran/probes/messages/ErrorMessages.java
+++ src/main/java/telran/probes/messages/ErrorMessages.java
@@ -4,6 +4,7 @@ public interface ErrorMessages {
 
 	String MISSING_SENSOR_ID = "Missing sensor id";
 	String MISSING_EMAILS = "Missing email addresses for sensor";
+	String EMPTY_EMAILS = "Received empty list of emails";
 	String MISSING_RANGE = "Missing range values for sensor";
 	String MISSING_MIN_VALUE = "Missing min value in range";
 	String MISSING_MAX_VALUE = "Missing max value in range";
