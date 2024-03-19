94001853c38826c10a327794fcf1467902bbacd5
allanteone <allanteone@gmail.com>
2024-03-19 22:59:57 +0200

    hw_74a updates dto

--- src/main/java/telran/probes/dto/SensorUpdateData.java
+++ src/main/java/telran/probes/dto/SensorUpdateData.java
@@ -1,5 +1,30 @@
 package telran.probes.dto;
 
+import java.util.Arrays;
+import java.util.Objects;
+
 public record SensorUpdateData(long id, Range range, String[] emails) {
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + Arrays.hashCode(emails);
+		result = prime * result + Objects.hash(id, range);
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		SensorUpdateData other = (SensorUpdateData) obj;
+		return Arrays.equals(emails, other.emails) && id == other.id && Objects.equals(range, other.range);
+	}
+	
 
 }
