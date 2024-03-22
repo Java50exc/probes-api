a70069b51cdd6d10deb0f4669af31b384d346ff8
allanteone <allanteone@gmail.com>
2024-03-22 17:54:17 +0200

    hw_78 fixes url constants

--- src/main/java/telran/probes/UrlConstants.java
+++ src/main/java/telran/probes/UrlConstants.java
@@ -1,8 +1,8 @@
 package telran.probes;
 
 public interface UrlConstants {
-	String SENSOR_RANGE_PATH = "range/sensor/";
-	String EMAILS_PATH = "emails/sensor/";
+	String SENSOR_RANGE_PATH = "range/sensor";
+	String EMAILS_PATH = "emails/sensor";
 	
 
 }
