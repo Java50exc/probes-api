0c1179345243ddfd6877bd0a0e186bef46b65759
allanteone <allanteone@gmail.com>
2024-03-22 17:54:05 +0200

    hw_78 adds new error messages

--- src/main/java/telran/probes/messages/ErrorMessages.java
+++ src/main/java/telran/probes/messages/ErrorMessages.java
@@ -8,7 +8,11 @@ public interface ErrorMessages {
 	String MISSING_RANGE = "Missing range values for sensor";
 	String MISSING_MIN_VALUE = "Missing min value in range";
 	String MISSING_MAX_VALUE = "Missing max value in range";
+	String RANGE_MIN_GREATER_THEN_MAX = "Max value must be greater then min value";
+	String INVALID_EMAIL_FORMAT = "Email format is not valid: ";
+	String EMAIL_REGEX = "^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\\.[a-zA-Z]{2,}$";
 	
 	String SENSOR_RANGE_ALREADY_EXISTS = "Range for a given sensor already exists";
 	String SENSOR_EMAILS_ALREADY_EXISTS = "Emails for a given sensor already exists";
+	
 }
